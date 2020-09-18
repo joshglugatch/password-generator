819aaddf195e1d96999af2a7c75a00ab349b20af
Josh Glugatch <joshglugatch@gmail.com>
2020-09-18 15:31:27 -0700

    add restrictions to password length prompt

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ var generateBtn = document.querySelector("#generate");
 
 //create password length function
 function howLong(){
-  var response = prompt("How many characters would you like your password to be?") 
+  var response = prompt("How many characters would you like your password to be?");
+  if(response === null){
+    return;
+  }
   if(response < 8 || response > 128){
-    alert("Password must be between 8 and 128 characters.")
+    alert("Password must be between 8 and 128 characters.");
+    howLong()
+  } 
+  if(response !== Number){
+    alert("Input may only contain numbers.")
     howLong()
   }
+  console.log(response);
 }
 // run password length function
 howLong()
+
+//password criteria
+
 
 // Write password to the #password input
 function writePassword() {
