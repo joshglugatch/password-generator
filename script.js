51e947dfea9ef1ad2b1dba2b85cdf912bec31bfb
Josh Glugatch <joshglugatch@gmail.com>
2020-09-18 15:03:18 -0700

    create working password length function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+//create password length function
+function howLong(){
+  var response = prompt("How many characters would you like your password to be?") 
+  if(response < 8 || response > 128){
+    alert("Password must be between 8 and 128 characters.")
+    howLong()
+  }
+}
+// run password length function
+howLong()
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +24,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
