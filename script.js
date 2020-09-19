266d47d97a7dd8e11ea62824166303fd9371559d
Josh Glugatch <joshglugatch@gmail.com>
2020-09-19 12:21:35 -0700

    change i=0 to i=1 in for loop to output correct number of characters

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-// Write password to the #password input
+//generate password function
 
 function generatePassword(){
   
   // password length 
-  
   var response = prompt("How many characters would you like your password to be?");
-  //console.log(response)
+  
   if(response === null){
     return;
   }
@@ -25,21 +24,16 @@ function generatePassword(){
   
   response = parseInt(response)
   console.log(response)
-  // password criteria
-  // var upperValues = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  // var lowerValues = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  // var numberValues = ["1","2","3","4","5","6","7","8","9","0"];
-  // var symbolValues = ["!","@","#","$","%","^","&","*","(",")","+","="];
-
+  
+ //criteria values
   var upperValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerValues = "abcdefghijklmnopqrstuvwxyz";
   var numberValues = "1234567890";
   var symbolValues = "!@#$%^&*()+=";
-
+  //new string with chosen inputs
+  pwString="";
   
-  alert("Choose your password character criteria.")
-  pwString=""
-
+  // password criteria 
   var upperCase = confirm("Click OK include capitalized letters.");
   if(upperCase){
     pwString += pwString.concat(upperValues)
@@ -56,17 +50,21 @@ function generatePassword(){
   if(symbols){
     pwString += pwString.concat(symbolValues)
     }
+  else{
+    alert("Please select valid criteria.")
+    return;
+  }
 
   
     
-  var passwordText = document.querySelector("#password");
+  //random generation
   var password = "";
-   //random generation
-  for(var i = 0; i <= response; i++){
-  password = password + pwString.charAt(Math.floor(Math.random() * Math.floor(pwString.length - 1)))
-  
+  for(var i = 1; i <= response; i++){
+  password = password + pwString.charAt(Math.floor(Math.random() * Math.floor(pwString.length)))
   }
+
   //add to text
+  var passwordText = document.querySelector("#password");
   passwordText.value = password
   console.log(password)
 }
