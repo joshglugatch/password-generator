e0fee3c984cb3bfa8808e40c6675d9889177a925
Josh Glugatch <joshglugatch@gmail.com>
2020-09-18 19:30:23 -0700

    add password criteria

--- script.js
+++ script.js
@@ -1,36 +1,66 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var pwArray = []
 
 
-//create password length function
-function howLong(){
+// Write password to the #password input
+
+function writePassword(){
+  
+  // password length 
   var response = prompt("How many characters would you like your password to be?");
+  console.log(response)
   if(response === null){
     return;
   }
   if(response < 8 || response > 128){
-    alert("Password must be between 8 and 128 characters.");
-    howLong()
+    alert("Password must be a number between 8 and 128 characters.");
+    return;
   } 
-  if(response !== Number){
+  if(isNaN(response)){
     alert("Input may only contain numbers.")
-    howLong()
+    return;
+  } 
+
+  // password criteria
+  var upperValues = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerValues = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numberValues = ["1","2","3","4","5","6","7","8","9","0"];
+  var symbolValues = ["!","@","#","$","%","^","&","*","(",")","+","="];
+  
+  alert("Choose your password character criteria.")
+  var upperCase = confirm("Do you want your password to include capitalized letters? (Click OK for yes, and CANCEL for no)");
+  var lowerCase = confirm("Do you want your password to include lower case letters? (Click OK for yes, and CANCEL for no)");
+  var numbers = confirm("Do you want your password to include numbers? (Click OK for yes, and CANCEL for no)");
+  var symbols = confirm("Do you want your password to include symbols? (Click OK for yes, and CANCEL for no)");
+
+  if(upperCase){
+  pwArray.push(...upperValues)
   }
-  console.log(response);
+  if(lowerCase){
+  pwArray.push(...lowerValues)
+  }
+  if(numbers){
+  pwArray.push(...numberValues)
+  }
+  if(symbols){
+  pwArray.push(...symbolValues)
+  }
 }
-// run password length function
-howLong()
 
-//password criteria
+var password = generatePassword();
 
+function generatePassword(){
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
+
+  for(var i=0, i < pwArray.length, i++){
+
+  }
+
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
